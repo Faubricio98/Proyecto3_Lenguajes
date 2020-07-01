0143092da56bc3406a86b6e94508fe61da0e892d
Faubricio Chaves <faubricio98@gmail.com>
2020-06-30 23:45:55 -0600

    Vista del inicio de sesion y parte del modulo Admin

---
+++ Controllers/AdministradorController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Proyecto3_Lenguajes.Controllers
+{
+    public class AdministradorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
